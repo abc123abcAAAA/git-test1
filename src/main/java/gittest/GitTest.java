30af9d477cecb0efce34100e3e1ceb583172dc42
zhangsan <zhangsan@163.com>
2022-09-03 12:36:47 +0800

    master push commit

--- src/main/java/gittest/GitTest.java
+++ src/main/java/gittest/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest {
         System.out.println("hello git I am hot-fix");
         //在master合并hot-fix之前，对master分支做出一定修改
         System.out.println("master test");
-
+        System.out.println("push master");
         System.out.println("hello git I am hot-fix2");
     }
 }
